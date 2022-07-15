fec9991652c79e90d2a7897bf64d118765d77294
cecialveslima <cecialveslima@gmail.com>
2022-07-15 09:13:52 -0300

    Atualizar

---
+++ src/app/components/bookstore-app/product-list/product-list.component.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import {HttpClient, HttpHeaders} from "@angular/common/http"
+import { Book } from "./model/book";
+
+
+@Injectable()
+export class BookService{
+    private url = "sua url aqui"
+    httpOptions = {
+        Headers: new HttpHeaders({
+            'Content-Type': 'application/json'
+        })
+    }
+
+    constructor(private http: HttpClient){}
+
+
+    getBook(){
+        return this.http.get(this.url)
+    }
+}
